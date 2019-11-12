fa539e549c6e55718457bd73cc9dc9c42678862f
Pavel Marchanka <Pavel_Marchanka@epam.com>
2019-11-12 16:34:30 +0300

    upd

--- VideoPortal/src/app/app.component.spec.ts
+++ VideoPortal/src/app/app.component.spec.ts
@@ -1,17 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import * as core from './core/core.module';
-import { VpHeaderComponent } from './core/vp-header/vp-header.component';
-import { VpBreadcrumbsComponent } from './core/vp-breadcrumbs/vp-breadcrumbs.component';
-import { VpSectionComponent } from './core/vp-section/vp-section.component';
-import { VpListCourcesComponent } from './core/vp-list-cources/vp-list-cources.component';
-import { VpCourceComponent } from './core/vp-cource/vp-cource.component';
-import { VpFooterComponent } from './core/vp-footer/vp-footer.component';
-import { VpLogoComponent } from './core/vp-logo/vp-logo.component';
-import { VpSearchCourceComponent } from './core/vp-list-cources/vp-search-cource/vp-search-cource.component';
-import { VpAddCourceComponent } from './core/vp-list-cources/vp-add-cource/vp-add-cource.component';
-import { VpLoadMoreComponent } from './core/vp-list-cources/vp-load-more/vp-load-more.component';
+
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -20,9 +11,9 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent , VpHeaderComponent, VpBreadcrumbsComponent, VpSectionComponent, VpListCourcesComponent,
-        VpCourceComponent, VpFooterComponent, VpLogoComponent, VpSearchCourceComponent, VpAddCourceComponent, VpLoadMoreComponent
+        AppComponent 
       ],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
 
